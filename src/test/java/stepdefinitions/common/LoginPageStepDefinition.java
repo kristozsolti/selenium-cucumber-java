9124bbc21befe25d7f7c409ac205c0d0261dd2bd
kristozsolti <kristo.zsolti@gmail.com>
2019-05-04 14:11:35 +0300

    Login feature cleanup

---
+++ src/test/java/stepdefinitions/common/LoginPageStepDefinition.java
@@ -0,0 +1,58 @@
+package stepdefinitions.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.mylibrary.pages.Pages;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginPageStepDefinition {
+	
+//	Scenario1
+	@Given("^Application Login screen when invalid$")
+	public void applicationLoginScreen() throws Throwable {
+		Pages.loginPage().goTo();
+	}
+
+	@When("^I enter wrong credentials$")
+	public void iEnterMyInvalidCredentials() throws Throwable {
+		String username = "invalid_email@ex.com";
+		String password = "randompassword";
+		
+		Pages.loginPage().setUsernameField(username);
+		Pages.loginPage().setPasswordField(password);
+	}
+
+	@And("^Click to login button when invalid$")
+	public void clickToLoginButton() throws Throwable {
+		Pages.loginPage().clickToLoginButton();
+	}
+
+	@Then("^I Should not be authenticated and get error message$")
+	public void iShouldNotBeAuthenticatedAndGetErrorMessage() throws Throwable {
+		String error_msg = "Check your username and password and try again.";
+		String errorMessage = Pages.loginPage().wrongCredentialsError();
+		assertEquals(errorMessage, error_msg);
+	}
+
+//	Scenario 2
+	private static String USERNAME = "";
+
+	@When("^I enter my ([^\"]*) and ([^\"]*)$")
+	public void iEnterMyValidCredentials(String username, String password) throws Throwable {
+		Pages.loginPage().setUsernameField(username);
+		Pages.loginPage().setPasswordField(password);
+		USERNAME = username;
+	}
+
+	@Then("^I Should be authenticated$")
+	public void iShouldBeAuthenticated() throws Throwable {
+		Boolean isUserAuthenticated = Pages.loginPage().isUserAuthenticated(USERNAME);
+		assertTrue(isUserAuthenticated);
+	}
+	
+}
